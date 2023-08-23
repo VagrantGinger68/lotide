27dd12a854951f2cbd739305325d70324424e03a
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-23 18:26:06 -0400

    Made a function that returns the middle-most variable in an array

---
+++ middle.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  //check that arrays are same length
+  if (arr1.length !== arr2.length) {
+    result = false;
+  } else {
+    //check that objects are the same
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr2[i]) {
+        result = true;
+      } else {
+        result = false;
+      }
+    }
+  }
+  return result;
+};
+
+const middle = function(array) {
+  let middleArray = [];
+  if (array.length <= 2) {
+    return middleArray;
+  } else if (array.length % 2 === 0) {
+    middleArray = [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    middleArray = [array[Math.floor(array.length / 2)]];
+  }
+  return middleArray;
+};
+console.log(middle([1,2,3,4,5,6]));
+
+
+
+
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array.length === 2) {
+//     middleArray = [];
+//   } else {
+//     middleArray += array[(array.length / 2)];
+//   }
+// }
+// console.log(middleArray);
