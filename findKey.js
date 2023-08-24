eace2dd56b7714245837a98a2ee7a6124f36e824
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-24 18:57:51 -0400

    Made a function that finds keys in an object through a callback function

---
+++ findKey.js
@@ -0,0 +1,39 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Make a function that takes in an object and a callback
+const findKey = function(object, callback) {
+  //Loop through object to get the keys
+  for (let key in object) {
+    //If the callback returns true with the object key, return the key
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  //If no key is found, return undefined
+  return undefined;
+}
+
+// findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2); // => "noma"
+
+//Check if arrays are equal
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
