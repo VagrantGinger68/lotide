4be61196a7bd90a7fc4664b215a53e66c2be3905
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-28 18:43:02 -0400

    added a test description

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,4 +1,8 @@
-const assertEqual = require("../assertEqual");
+const assert = require('chai').assert;
 const eqArrays = require("../eqArrays");
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+});
