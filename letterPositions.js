0f95cbeb4ad4ba1fe5c08d694b9cf02adef3935e
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-28 17:21:19 -0400

    refactored to be modules and added Mocha test

--- letterPositions.js
+++ letterPositions.js
@@ -1,32 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let result = true;
-  //check that arrays are same length
-  if (arr1.length !== arr2.length) {
-    result = false;
-  } else {
-    //check that objects are the same
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        result = true;
-      } else {
-        result = false;
-      }
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  //call eqArrays function and check if arrays are equal
-  if (eqArrays(arr1,arr2) === true) {
-    //Log a success message
-    console.log("✅✅✅Assertion Passed: Arrays are identical!");
-  } else {
-    //Log a failure message
-    console.log("🛑🛑🛑Assertion Failed: Arrays are not the same.");
-  }
-};
-
 const letterPositions = function(sentence) {
   let results = {};
 
@@ -43,4 +14,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-console.log(letterPositions("Hello"));
+module.exports = letterPositions;
--- map.js
+++ map.js
@@ -1,36 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  let result = true;
-  //check that arrays are same length
-  if (arr1.length !== arr2.length) {
-    result = false;
-  } else {
-    //check that objects are the same
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        result = true;
-      } else {
-        result = false;
-      }
-    }
-  }
-  return result;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  //call eqArrays function and check if arrays are equal
-  if (eqArrays(arr1,arr2) === true) {
-    //Log a success message
-    console.log("✅✅✅Assertion Passed: Arrays are identical!");
-  } else {
-    //Log a failure message
-    console.log("🛑🛑🛑Assertion Failed: Arrays are not the same.");
-  }
-};
-
-
-
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -39,7 +6,4 @@ const map = function(array, callback) {
   return results;
 };
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
-
-assertArraysEqual(map(words, word => word[0]), map(words, word => word[0]));
+module.exports = map;
