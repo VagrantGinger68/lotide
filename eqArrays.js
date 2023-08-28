37594e4f04b36ada3f58234838ca6e1351f576de
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-28 14:54:06 -0400

    refactored eqArrays to be modular and added test file

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(arr1, arr2) {
   let result = true;
   //check that arrays are same length
@@ -24,4 +16,4 @@ const eqArrays = function(arr1, arr2) {
   return result;
 };
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
