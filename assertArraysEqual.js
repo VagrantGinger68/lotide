069d6eb47258022df3736e0b3dbd77d593bc7749
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-28 15:04:00 -0400

    required eqArrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,28 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  let result = true;
-  //check that arrays are same length
-  if (arr1.length !== arr2.length) {
-    result = false;
-  } else {
-    //check that objects are the same
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] === arr2[i]) {
-        result = true;
-      } else {
-        result = false;
-      }
-    }
-  }
-  return result;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(arr1, arr2) {
   //call eqArrays function and check if arrays are equal
@@ -35,4 +11,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1,2, 3], [1,2,3]);
+module.exports = assertArraysEqual;
