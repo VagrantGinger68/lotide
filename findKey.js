40deb25b397efda1c5639903f1a7007a4f67b86e
Ryan Schelhaas <ryan.schelhaas@gmail.com>
2023-08-28 18:41:36 -0400

    refactored to me modular and added Mocha tests

--- findKey.js
+++ findKey.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 //Make a function that takes in an object and a callback
 const findKey = function(object, callback) {
   //Loop through object to get the keys
@@ -19,21 +11,4 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-// findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2); // => "noma"
-
-//Check if arrays are equal
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), "noma");
+module.exports = findKey;
